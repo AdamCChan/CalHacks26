09c8096b97f10651bc9ca9ba8ab9b5b075849805
Lucas <kasdorflgm@gmail.com>
2026-02-15 13:03:36 -0700

    Add logout redirect

--- frontend/components/Footer.tsx
+++ frontend/components/Footer.tsx
@@ -15,61 +15,74 @@ export default function Footer() {
   const userPath = "/user";
   const friendsPath = "/friends";
   const settingsPath = "/settings";
+  const logoutPath = "/signin";
 
   const assetsFolderPath = "/assets";
 
   return (
     <footer className="footer">
-      <Link href={addCapsulePath}>
-        <Image
-          src={assetsFolderPath + "/add capsule.png"}
-          alt=""
-          width={28}
-          height={28}
-          className={iconClass(addCapsulePath)}
-        />
-      </Link>
+      <div className="footer-main">
+        <Link href={addCapsulePath}>
+          <Image
+            src={assetsFolderPath + "/add capsule.png"}
+            alt=""
+            width={28}
+            height={28}
+            className={iconClass(addCapsulePath)}
+          />
+        </Link>
 
-      <Link href={dashboardPath}>
-        <Image
-          src={assetsFolderPath + "/capsule search.png"}
-          alt=""
-          width={28}
-          height={28}
-          className={iconClass(dashboardPath)}
-        />
-      </Link>
+        <Link href={dashboardPath}>
+          <Image
+            src={assetsFolderPath + "/capsule search.png"}
+            alt=""
+            width={28}
+            height={28}
+            className={iconClass(dashboardPath)}
+          />
+        </Link>
 
-      <Link href={userPath}>
-        <Image
-          src={assetsFolderPath + "/user.png"}
-          alt=""
-          width={28}
-          height={28}
-          className={iconClass(userPath)}
-        />
-      </Link>
-      
-      <Link href={friendsPath}>
-        <Image
-          src={assetsFolderPath + "/social network.png"}
-          alt=""
-          width={28}
-          height={28}
-          className={iconClass(friendsPath)}
-        />
-      </Link>
-      
-      <Link href={settingsPath}>
-        <Image
-          src={assetsFolderPath + "/settings.png"}
-          alt=""
-          width={28}
-          height={28}
-          className={iconClass(settingsPath)}
-        />
-      </Link>
-      
+        <Link href={userPath}>
+          <Image
+            src={assetsFolderPath + "/user.png"}
+            alt=""
+            width={28}
+            height={28}
+            className={iconClass(userPath)}
+          />
+        </Link>
+
+        <Link href={friendsPath}>
+          <Image
+            src={assetsFolderPath + "/social network.png"}
+            alt=""
+            width={28}
+            height={28}
+            className={iconClass(friendsPath)}
+          />
+        </Link>
+
+        <Link href={settingsPath}>
+          <Image
+            src={assetsFolderPath + "/settings.png"}
+            alt=""
+            width={28}
+            height={28}
+            className={iconClass(settingsPath)}
+          />
+        </Link>
+      </div>
+      <div className="footer-logout">
+        <Link href={logoutPath}>
+          <Image
+            src={assetsFolderPath + "/logout.png"}
+            alt=""
+            width={28}
+            height={28}
+            className={iconClass(logoutPath)}
+          />
+        </Link>
+      </div>
     </footer>
   );
 }
